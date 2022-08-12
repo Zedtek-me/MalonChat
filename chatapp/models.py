bdca1de5a0fb398082ed83d0bbaa88ef6071549b
unknown <zechariahadebayo42@gmail.com>
2022-08-12 21:04:04 +0100

    created db tables to simulate wallets and tipping. Created a userprofile table too, for other attributes on the user like 'video_host', 'video_guest', etc

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# All Tables go here as python classes. I'm using Django ORM here, and I'm going by Django's Default 'User Model Here
+class UserProfile(models.Model):
+    '''
+    This extends the built in User table that comes with django, as its user profile, 
+    having other columns as given below.
+    '''
+    user= models.OneToOneField(on_delete=models.CASCADE)
+    video_host= models.BooleanField('Started a video', default=False)
+    video_guest= models.BooleanField('A video guest?', default=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+class Wallet(models.Model):
+    '''
+    A table implementing user wallet. 
+    It has a one-to-one relationship with the User table, cuze a user should only have one wallet
+    '''
+    owner= models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_name= models.CharField('user wallet\'s name', max_length=300, unique=True, null=False, blank=False)
+
+    def __str__(self) -> str:
+        '''
+        tells about the owner of the wallet.
+        '''
+        return f"{self.owner.username}'s wallet."
+
+
+class Tip(models.Model):
+    '''
+    This implements the tips table.
+    It has a many to one relationship with the 'Wallet' and 'User Table'. This is so because a wallet could have many tips,
+    and a "giver"(or User) could give More than a single tip as well.
+    '''
+    wallet= models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    giver= models.ForeignKey(User, on_delete=models.SET_NULL)
+    amount= models.IntegerField('Tip\'s worth', blank=False, null= False, unique=False)
+    date_given= models.DateTimeField('When tipped', auto_now_add=True)
+
+    def __str__(self):
+        '''
+        to return a string representation of the amount given as a tip.
+        '''
+        return self.amount
---
+++ chatapp/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from chatapp.models import UserProfile
+
+
+
+def create_profile(sender, created, instance, **kwargs):
+    '''
+    This would be the signal receiver.
+    It should automatically create a profile for the user once a user is created (just like an event listener).
+    All signals in django sends some properties of their senders along with them; hence, the parameter given above.
+    '''
+    if created:
+        profile= UserProfile.object.create(user= instance)
+
+
+post_save.connect(create_profile, User)
